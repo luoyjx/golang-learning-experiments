9d14da20045b2d0555931d0a0eb21fc0692d071c
luoyjx <yanjixiong@shimo.im>
2020-12-28 09:43:23 +0800

    chore: http test using URL with blank char

---
+++ http-client-test/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "http://shimo.f3322.net:3000/static/files/蔡诗 音(名字带空格).doc"
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Header.Get("Content-Type"))
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(b))
+}
