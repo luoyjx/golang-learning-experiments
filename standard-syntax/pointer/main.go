2bb664a959c3507efd345ac4722bcd850aa0dc63
yanjixiong <yanjixiong@shimo.im>
2022-07-08 10:14:38 +0800

    chore: standard syxtax

--- standard-syntax/pointer/main.go
+++ standard-syntax/pointer/main.go
@@ -1,14 +1,14 @@
 package main
 
+import "fmt"
+
 type A struct {
-	Name string
-	Age int
+	Name   string
+	Age    int
 	Parent *A
 }
 
 func main() {
 	a := new(A)
-
-	a->Parent = new(A)
 	fmt.Println(a)
 }
