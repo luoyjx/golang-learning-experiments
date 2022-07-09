f89d4ca8811e012508e3be8644a8bfb1df233e42
yanjixiong <yanjixiong@shimo.im>
2022-07-09 12:06:36 +0800

    chore: fmt print struct with default format

--- standard-library/fmt/main.go
+++ standard-library/fmt/main.go
@@ -19,4 +19,22 @@ func main() {
 	}
 
 	fmt.Println(string(bs))
+	fmt.Println("print Foo struct JSON bytes as string")
+	fmt.Printf("print Foo JSON bytes string with fmt.Printf %s\n", string(bs))
+	fmt.Println("fmt print format reference => https://pkg.go.dev/fmt")
+
+	printAsDefaultStructFormat(f)
+	printWithStructFieldName(f)
+}
+
+func printAsDefaultStructFormat(f Foo) {
+	// print:
+	// default struct format: {barvalue}
+	fmt.Printf("default struct format: %v\n", f)
+}
+
+func printWithStructFieldName(f Foo) {
+	// print:
+	// struct field name: {Bar:barvalue}
+	fmt.Printf("struct field name: %+v\n", f)
 }
