71f145ec8c4ccac2b04bbcd06e1a9605332884d2
yanjixiong <yanjixiong@shimo.im>
2023-03-18 11:52:21 +0800

    chore: more zap basic usage with all log levels

--- third-party-library/logging/zap/basic/basic.go
+++ third-party-library/logging/zap/basic/basic.go
@@ -8,9 +8,15 @@ func main() {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync() // Flush any buffered log entries
 
-	// Log a simple message
-	logger.Info("Zap logger basic usage example")
+	// Log a simple message at different log levels
+	logger.Debug("Zap logger debug level")
+	logger.Info("Zap logger info level")
+	logger.Warn("Zap logger warn level")
+	logger.Error("Zap logger error level")
 
-	// Log a message with fields
-	logger.Info("Zap logger with fields", zap.String("key", "value"), zap.Int("number", 42))
+	// Log a message with fields at different log levels
+	logger.Debug("Zap logger with fields at debug level", zap.String("key", "value"), zap.Int("number", 42))
+	logger.Info("Zap logger with fields at info level", zap.String("key", "value"), zap.Int("number", 42))
+	logger.Warn("Zap logger with fields at warn level", zap.String("key", "value"), zap.Int("number", 42))
+	logger.Error("Zap logger with fields at error level", zap.String("key", "value"), zap.Int("number", 42))
 }
